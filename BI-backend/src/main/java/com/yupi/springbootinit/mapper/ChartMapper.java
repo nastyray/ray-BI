ee3ec70c2ddf809b92daa12a3ca7aa33ef51234d
ray <2313715475@qq.com>
2023-12-23 14:26:22 +0800

    后端
    后端代码

---
+++ BI-backend/src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,19 @@
+package com.yupi.springbootinit.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.Chart;
+
+/**
+* @author Lenovo
+* @description 针对表【chart(图表信息表)】的数据库操作Mapper
+* @createDate 2023-12-22 17:03:21
+* @Entity generator.domain.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
---
+++ BI-backend/src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.User;
+
+/**
+* @author Lenovo
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2023-12-22 17:03:21
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
